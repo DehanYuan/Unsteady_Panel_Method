c5272be5fff282a5a3c999530fe92f0d074fd9f5
Dehan Yuan <95612391+DehanYuan@users.noreply.github.com>
2022-05-10 14:11:17 +0800

    Update TSlxTSly.py

--- TSlxTSly.py
+++ TSlxTSly.py
@@ -5,18 +5,14 @@ import math as math
 np.seterr('raise')
 
 
-def TSlxTSly(xSl, ySl, xT, yT, Theta_T, DeltaS_T):
-    # Number of panels
-
-
-
+def TSlxTSly(x_isl, y_isl, xTB, yTB, Theta_T, DeltaS_T):
         # Compute intermediate values
-    A = -(xSl - xT) * np.cos(Theta_T) - (ySl - yT) * np.sin(Theta_T)  # A term
-    B = (xSl - xT) ** 2 + (ySl - yT) ** 2  # B term
+    A = -(x_isl - xTB) * np.cos(Theta_T) - (y_isl - yTB) * np.sin(Theta_T)  # A term
+    B = (x_isl - xTB) ** 2 + (y_isl - yTB) ** 2  # B term
     Cx = np.sin(Theta_T)  # Cx term (X-direction)
-    Dx = -(ySl - yT)  # Dx term (X-direction)
+    Dx = -(y_isl - yTB)  # Dx term (X-direction)
     Cy = -np.cos(Theta_T)  # Cy term (Y-direction)
-    Dy = xSl - xT  # Dy term (Y-direction)
+    Dy = x_isl - xTB  # Dy term (Y-direction)
     E = math.sqrt(B - A ** 2)  # E term
 
     if (E == 0 or np.iscomplex(E) or np.isnan(E) or np.isinf(E)):  # If E term is 0 or complex or a NAN or an INF
